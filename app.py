e4ab19966065e238dc6a00ee69fa2e30b4b6cd39
maharishi2019 <55564095+maharishi2019@users.noreply.github.com>
2021-02-13 13:00:09 -0800

    added state search

--- app.py
+++ app.py
@@ -10,25 +10,35 @@ app = Flask(__name__)
 def index():
     if request.method == "POST":
         country_input = request.form["country"]
+        country_input_modified = request.form["country"].lower().replace(" ", "-")
         state_input = request.form["state"]
         country_covid_api = requests.get(
             "https://api.covid19api.com/summary").json()
 
         state_covid_api = requests.get(
-            "https://api.covidtracking.com/v1/states/%s").json()
+            "https://api.covid19api.com/live/country/%s" % (country_input_modified)).json()
 
-        covidInformation = None
+        countryCovidInformation = None
+        stateCovidInformation = None
         # exceptions
         if(country_input == "United States"):
+            
             country_input = "United States of America"
-
-        print(state_covid_api)
 
         for i in country_covid_api["Countries"]:
             if(i['Country'] == country_input):
-                covidInformation = i
-        if(covidInformation):
-            return render_template("find.html", apiConnection=True, countrySearch=True, covidInformation=covidInformation)
+                countryCovidInformation = i
+
+        for i in state_covid_api:
+            if(i['Province'] == state_input):
+                stateCovidInformation = i; 
+
+        if(countryCovidInformation and stateCovidInformation):
+            return render_template("find.html", apiConnection=True, countrySearch=True, stateSearch=True, countryCovidInformation=countryCovidInformation, stateCovidInformation=stateCovidInformation)
+        elif(countryCovidInformation):
+            return render_template("find.html", apiConnection=True, countrySearch=True, countryCovidInformation=countryCovidInformation)
+        elif(stateCovidInformation):
+            return render_template("find.html", apiConnection=True, stateSearch=True, stateCovidInformation=stateCovidInformation)
         else:
             return render_template('find.html', apiConnectionFailed=True)
     return render_template("find.html")
