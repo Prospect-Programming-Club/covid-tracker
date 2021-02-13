7cad5ab0260a5e2500adda251754455a9092ab91
maharishi2019 <55564095+maharishi2019@users.noreply.github.com>
2021-02-13 11:37:13 -0800

    added css, search country information

--- app.py
+++ app.py
@@ -1,11 +1,29 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request, flash
 import requests
-
+import json
+import pycountry
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", methods=["POST", "GET"])
 def index():
+    if request.method == "POST":
+        country_input = request.form["country"]
+        country_covid_api = requests.get(
+            "https://api.covid19api.com/summary").json()
+
+        covidInformation = None
+        # exceptions
+        if(country_input == "United States"):
+            country_input = "United States of America"
+
+        for i in country_covid_api["Countries"]:
+            if(i['Country'] == country_input):
+                covidInformation = i
+        if(covidInformation):
+            return render_template('find.html', apiConnection=True, covidInformation=covidInformation)
+        else:
+            return render_template('find.html', apiConnectionFailed=True)
     return render_template("find.html")
 
 
